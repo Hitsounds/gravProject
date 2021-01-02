b06a241ab98172ff3b9e35eb874ba80b3bf0107e
Marvel Renju <marvelrenju1@gmail.com>
2021-01-02 21:33:55 +0000

    Basic Layout
    The four corners.

--- main.py
+++ main.py
@@ -1,22 +1,79 @@
-from tkinter import Tk, Label, Button
+import tkinter as tk
 
-class SimGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
+class SimulationViewer(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.config(bg="red")
+        test = tk.Label(self, text="SimulationViewer" )
+        test.pack()
 
-        self.label = Label(master, text="Test GUI!")
-        self.label.pack()
+class Timeline(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.config(bg="blue")
+        test = tk.Label(self, text="Timeline" )
+        test.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+class SimConfig(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.config(bg="purple")
+        test = tk.Label(self, text="SimConfig" )
+        test.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+class VariableViewer(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.config(bg="green")
+        test = tk.Label(self, text="VariableViewer")
+        test.pack()
 
-    def greet(self):
-        print("Greetings!")
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.parent.title("Gravity Simulator")
+        self.parent.geometry("1024x768")
 
-root = Tk()
-my_gui = SimGUI(root)
-root.mainloop()
+        #Setting relative sizes using weights
+        self.columnconfigure(0, weight=7)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=5)
+        self.rowconfigure(1, weight=1)
+
+        #Create Frame objects for the GUI
+        self.viewer = SimulationViewer(self)
+        self.timeline = Timeline(self)
+        self.SimConfig = SimConfig(self)
+        self.VariableViewer = VariableViewer(self)
+
+        #Use grid GUI layout to organise the frames according to design
+        self.viewer.grid(row=0,
+                        column=0,
+                        pady=20,
+                        padx=20,
+                        sticky="NWSE")
+        self.timeline.grid(row=1,
+                        column=0,
+                        pady=20,
+                        padx=20,
+                        sticky="NWSE")
+        self.SimConfig.grid(row=0,
+                        column=1,
+                        pady=20,
+                        padx=20,
+                        sticky="NWSE")
+        self.VariableViewer.grid(row=1,
+                        column=1,
+                        pady=20,
+                        padx=20,
+                        sticky="NWSE")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
