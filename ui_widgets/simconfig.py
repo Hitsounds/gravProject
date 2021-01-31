8d6ef12e43992472a3ad5142ddb2d851ab6975fa
Marvel Renju <marvelrenju1@gmail.com>
2021-01-31 23:33:22 +0000

    Add nickname field to bodies

--- ui_widgets/simconfig.py
+++ ui_widgets/simconfig.py
@@ -12,7 +12,7 @@ class SimConfig(tk.Frame):
         self.settings = {
         "general":{
             "simulation_mode":["Simulation Mode","textChoice",tk.StringVar()],
-            "timestep":["Timestep Ratio","numInput",tk.StringVar()]
+            "timestep":["Timestep Ratio","numInput",tk.DoubleVar()]
         },
         "bodies":[]
         }
@@ -69,6 +69,8 @@ class SimConfig(tk.Frame):
 
     def floatValidation(self,test):
         try:
+            if test == "" or test == "-":
+                return True
             float(test)
             return True
         except Exception:
@@ -81,19 +83,24 @@ class SimConfig(tk.Frame):
         menu.delete(0, "end")
         menu.add_command(label="General", command=partial(self.render_generalsettings))
         for i in range(0, len(self.settings["bodies"])):
-            menu.add_command(label=f"Body {i}", command=partial(self.render_bodysettings, f"{i}"))
+            name = self.settings["bodies"][i]["nick"][2].get() or f"Body {i}"
+            menu.add_command(label=name, command=partial(self.render_bodysettings, f"{i}"))
         self.configMenu.update()
 
     def add_body(self):
         '''Adds a body to the end of the settings
         '''
         #body template
+        if len(self.settings["bodies"]) == 7:
+            #do nothing if user tries to add more than 7 bodies
+            return
         body_template = {
-                "mass":["Mass (kg)","numInput",tk.StringVar()],
-                "inital_x_pos":["x Position (m)","numInput",tk.StringVar()],
-                "inital_y_pos":["y Position (m)","numInput",tk.StringVar()],
-                "inital_x_speed":["x Speed (m/s)","numInput",tk.StringVar()],
-                "inital_y_speed":["y Speed (m/s)","numInput",tk.StringVar()]
+                "nick":["Nickname","stringInput",tk.StringVar()],
+                "mass":["Mass (kg)","numInput",tk.DoubleVar()],
+                "inital_x_pos":["x Position (m)","numInput",tk.DoubleVar()],
+                "inital_y_pos":["y Position (m)","numInput",tk.DoubleVar()],
+                "inital_x_speed":["x Speed (m/s)","numInput",tk.DoubleVar()],
+                "inital_y_speed":["y Speed (m/s)","numInput",tk.DoubleVar()]
         }  
         #defaults
         body_template["mass"][2].set(10)
@@ -107,6 +114,9 @@ class SimConfig(tk.Frame):
     def remove_body(self, i):
         '''Removes the body with the index provided
         '''
+        if len(self.settings["bodies"]) == 0:
+            #Do nothing if user tries to delete last body
+            return
         self.settings["bodies"].pop(i)
         self.construct_optionmenu()
 
@@ -120,6 +130,7 @@ class SimConfig(tk.Frame):
             widget.destroy()
 
     def render_generalsettings(self):
+        self.construct_optionmenu()
         self.choice.set("General")
         self.clear_optionsFrame()
 
@@ -138,44 +149,52 @@ class SimConfig(tk.Frame):
         timeStepInput.grid(row=1, column=1)
 
     def render_bodysettings(self, i):
-        self.choice.set(f"Body {i}")
+        self.construct_optionmenu()
+        name = self.settings["bodies"][int(i)]["nick"][2].get() or f"Body {i}"
+        self.choice.set(name)
         self.active_body = int(i)
         self.clear_optionsFrame()
 
+        #NickName
+        label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["nick"][0])
+        label.grid(row=0, column=0)
+        nickInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["nick"][2])
+        nickInput.grid(row=0, column=1)
+
         #Mass
         label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["mass"][0])
-        label.grid(row=0, column=0)
+        label.grid(row=1, column=0)
         massInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["mass"][2], validate="key")
-        massInput.configure(validatecommand=(massInput.register(self.floatValidation), "%P"))
-        massInput.grid(row=0, column=1)
+        massInput.configure(validatecommand=(massInput.register(self.positiveFloatValidation), "%P"))
+        massInput.grid(row=1, column=1)
 
         #x pos
         label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["inital_x_pos"][0])
-        label.grid(row=1, column=0)
+        label.grid(row=2, column=0)
         x_PosInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["inital_x_pos"][2], validate="key")
         x_PosInput.configure(validatecommand=(x_PosInput.register(self.floatValidation), "%P"))
-        x_PosInput.grid(row=1, column=1)
+        x_PosInput.grid(row=2, column=1)
 
         #y pos
         label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["inital_y_pos"][0])
-        label.grid(row=2, column=0)
+        label.grid(row=3, column=0)
         y_PosInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["inital_y_pos"][2], validate="key")
         y_PosInput.configure(validatecommand=(y_PosInput.register(self.floatValidation), "%P"))
-        y_PosInput.grid(row=2, column=1)
+        y_PosInput.grid(row=3, column=1)
 
         #x speed
         label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["inital_x_speed"][0])
-        label.grid(row=3, column=0)
+        label.grid(row=4, column=0)
         x_SpeedInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["inital_x_speed"][2], validate="key")
         x_SpeedInput.configure(validatecommand=(x_SpeedInput.register(self.floatValidation), "%P"))
-        x_SpeedInput.grid(row=3, column=1)
+        x_SpeedInput.grid(row=4, column=1)
 
         #y speed
         label = tk.Label(self.options,text=self.settings["bodies"][self.active_body]["inital_y_speed"][0])
-        label.grid(row=4, column=0)
+        label.grid(row=5, column=0)
         y_SpeedInput = tk.Entry(self.options, textvariable=self.settings["bodies"][self.active_body]["inital_y_speed"][2], validate="key")
         y_SpeedInput.configure(validatecommand=(y_SpeedInput.register(self.floatValidation), "%P"))
-        y_SpeedInput.grid(row=4, column=1)
+        y_SpeedInput.grid(row=5, column=1)
 
         #Remove body script
         def remove():
@@ -184,3 +203,6 @@ class SimConfig(tk.Frame):
 
         button = tk.Button(self.options, text="Remove Body", command=remove)
         button.grid(row=5, column=0)
+
+    def get_settings(self):
+        return self.settings
